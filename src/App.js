f7e7ea2503856c071d2e7f7471467540a66e5df0
YURY POPOV <75533mail@gmail.com>
2022-08-12 12:57:38 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,86 @@
-
+import { useState } from 'react';
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  console.log(calc);
+  console.log(result);
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    if (
+      ops.includes(value) && calc === '' ||
+      ops.includes(value) && ops.includes(calc.slice(-1))
+    ) {
+      return;
+    }
+
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  }
+ 
+  // Making a buttons from 1 to 9
+  const createDigits = () => {
+    const digits = []; 
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button 
+          onClick={() => updateCalc(i.toString())} 
+          key={i}>
+          {i}
+        </button>
+      )
+    }
+    return digits;
+  }
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
+
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+
+    setCalc(value);
+  }
+
+
   return (
     <div className="App">
-      Hello, World!
+      <div className="calculator">
+        <div className="display">
+          {result ? <span>({result})</span> : ''}&nbsp;
+          { calc || "0"}
+        </div>
+
+        <div className="operators">
+          <button onClick={() => updateCalc('/')}>/</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('+')}>+</button>
+          <button onClick={() => updateCalc('-')}>-</button>
+
+          <button onClick={deleteLast}>DEL</button>
+        </div>
+
+        <div className="digits">
+          { createDigits() }
+          <button onClick={() => updateCalc('0')}>0</button>
+          <button onClick={() => updateCalc('.')}>.</button>
+
+          <button onClick={calculate}>=</button>
+        </div>
+
+      </div>
     </div>
   );
 }
